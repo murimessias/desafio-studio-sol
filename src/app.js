a1f6172b09858812f5393db8e6ff102caef9e318
Murilo Messias <murilo.messias@gmail.com>
2021-10-18 22:41:51 -0300

    :lipstick: Add font files + Update styles

---
+++ src/app.js
@@ -0,0 +1,4 @@
+document.querySelector('[data-js="app"]').innerHTML = `
+  <h1>Studio Sol!</h1>
+  <p>Adivinhe o número</p>
+`
--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-import './styles/style.css'
+import './styles/global.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
-  <h1>Studio Sol!</h1>
-  <p>Adivinhe o número</p>
-`
+import './app'
